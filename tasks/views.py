571134768e4a02f4aa54c38fa1ed5e16fe9a5ebb
nick.drishinski <ndrishinski@gmail.com>
2024-03-04 09:15:34 -0500

    updated  to use request session for list

--- tasks/views.py
+++ tasks/views.py
@@ -3,16 +3,17 @@ from django import forms
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-tasks = ['foo', 'bar', 'baz']
-
 class NewTaskForm(forms.Form):
   task = forms.CharField(label="New Task")
   priority = forms.IntegerField(label="Priority", min_value=1, max_value=1)
 
 # Create your views here.
 def index(request):
+  if 'tasks' not in request.session:
+    request.session['tasks'] = []
+
   return render(request, "tasks/index.html", {
-    'tasks': tasks
+    'tasks': request.session['tasks']
   })
 
 def add(request):
@@ -20,7 +21,7 @@ def add(request):
     form = NewTaskForm(request.POST)
     if form.is_valid():
       task = form.cleaned_data['task']
-      tasks.append(task)
+      request.session['tasks'] += [task]
       return HttpResponseRedirect(reverse('tasks:index'))
     else:
       return render(request, "tasks/add.html", {
